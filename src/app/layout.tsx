43a485477f3cbe61cd46a5abfc810406b54eda57
mihir karbelkar <karbelkar.mihir@gmail.com>
2024-09-01 20:46:19 +0530

    Add analytics umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="6633bc32-b964-463d-be25-af7c4b7b3c5f"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
