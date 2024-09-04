8abe51d2c023f6f10a5ea20d71267768d18e6619
mihir karbelkar <karbelkar.mihir@gmail.com>
2024-09-05 02:51:03 +0530

    fix lint issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,21 +25,6 @@ export const typeOfComponent = (component: React.ReactElement) =>
  */
 export const getChildrenByType = (children: React.ReactNode, types: string[]) =>
   React.Children.toArray(children).filter(
-    (child) => !types.includes(typeOfComponent(child.type?.displayName))
+    (child) =>
+      !types.includes(typeOfComponent((child as any)?.type?.displayName))
   );
-
-/**
- * Ensures component type is not overwritten
- *
- * @param {string} expectedType - The expected type to validate against
- * @returns {func} Custom PropTypes validation function
- */
-export const typeValidation =
-  (expectedType) => (props, propName, componentName) => {
-    if (props[propName] !== expectedType) {
-      return new Error(
-        `'${propName}' in '${componentName}'\n\nYou may NOT pass in a prop value for '${propName}'. ` +
-          "It had 2 underscores in the prop name for a reason. So, if you would kindly remove it, we can all go about our error free day.\n"
-      );
-    }
-  };
