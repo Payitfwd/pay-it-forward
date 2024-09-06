b2f36509095948869435c65ad5732cede9a97bec
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2024-09-07 02:30:04 +0530

    feat: add cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,6 +3,15 @@
 declare global {
 	namespace App {
 		// interface Error {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
